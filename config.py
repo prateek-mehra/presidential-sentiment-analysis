0513e8b309ad856c882879acd971e8368a5577c6
prateek-mehra <63323034+prateek-mehra@users.noreply.github.com>
2021-01-19 20:01:08 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-consumer_key = "J9LhTFmi1I4UAHxju8CB6fODH"
-consumer_secret = "nQkp5yog57gVXAdjhWiIoLGEOKORjOiGxbRssN4PbztWwRdl6b"
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAPkfKgEAAAAAHle2mVblbBcqmJlo5QIc8mqFk4A%3DwPfC5TCBou4YQOce7ElC8H4AYcMaXWkAmGnOvDub7PQ6mlAG2t"
-
-key = "1165902298251284481-aPe20PfuFjTQYHDzCmRG6OGTFCdNuh"
-secret = "zoYtMZOudGLe1qpmUbTEgglCM8mhvIqLbS4ouzdAvoNgz"
